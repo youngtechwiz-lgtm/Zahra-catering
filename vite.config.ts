4927f573f75e1ab95eb95e8bb61374e30a451d4d
youngtechwiz-lgtm <youngtechwiz@gmail.com>
2026-09-12 09:05:09 -0700

    Configure Vercel SPA routing

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Zahra-catering',
+  base: '/',
 
   plugins: [
     react(),
